cd2c751f2402b2074a88662d1c055c19b23d222c
Alvaro Bezerra <alvaro.a.bezerra@hmit.com.br>
2025-01-22 14:17:36 -0300

    refresh token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,16 +1,28 @@
 import { AppError } from "@utils/AppError";
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
+
+type SignOut = () => void;
+
+type APIInstanceProps = AxiosInstance & {
+    registerInterceptTokenManager: (signOut: SignOut) => () => void;
+};
 
 const api = axios.create({
     baseURL: "http://172.16.21.184:3333",
-});
+}) as APIInstanceProps;
 
-api.interceptors.response.use(response => response, error => {
-    if(error.response && error.response.data) {
-        return Promise.reject(new AppError(error.response.data.message));
-    } else {
-        return Promise.reject(error);
-    }
-});
+api.registerInterceptTokenManager = signOut => {
+    const interceptTokenManager = api.interceptors.response.use(response => response, error => {
+        if(error.response && error.response.data) {
+            return Promise.reject(new AppError(error.response.data.message));
+        } else {
+            return Promise.reject(error);
+        }
+    });
+
+    return () => {
+        api.interceptors.response.eject(interceptTokenManager);
+    };
+};
 
 export { api };
